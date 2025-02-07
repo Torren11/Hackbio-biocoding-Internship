ce25569a00e3dd7680481256c28a3aa64f2cd3a4
Torren11 <oluwasolagraceajayi@gmail.com>
2025-02-08 00:25:31 +0100

    Add files via upload

---
+++ Stage_Zero/Stage_Zero_Task.R
@@ -0,0 +1,26 @@
+# LInks to Github profiles of teams members:
+#Rebecca Batoh: https://github.com/rbatoh/Hackbio-biocoding-internship.git
+#Emmanuel Oluwarotimi Igiehon: https://github.com/Rotimi626/
+#Ajayi Grace: https://github.com/Torren11/Hackbio-biocoding-internship.git
+#Olajide Temitope: https://github.com/TemitopeOlajide1/
+
+# Links to LinkedIn profiles of team members: 
+#Rebecca Batoh: https://www.linkedin.com/in/rebecca-batoh/
+#Emmanuel Oluwarotimi Igiehon: https://www.linkedin.com/in/emma-igiehon-300a2a260/
+#Ajayi Grace: https://www.linkedin.com/in/graceoluwasola/
+#Olajide Temitope: https://www.linkedin.com/in/temitope-esther-olajide/
+
+#Here is a detailed 
+team_data <- data.frame(
+  Name = c("Ajayi Grace","Laura Mazzucchi","Olajide Temitope","Rebecca Batoh","Emmanuel Oluwarotimi Igiehon","Osasenaga Oluwatosin Igiehon"),
+  Slack_Username = c("Grace","lauramazzu","Temitope","Becca","Emma","Osas"),
+  Email = c("oluwasolagraceajayi@gmail.com","lauramazzu.Im@gamil.com","te.olajide@gmail.com","rebeccabatoh17@gmail.com","emmanueligiehon80@gmail.com","osasenagaigiehon7@gmail.com"),
+  Hobby = c("Reading, learning new things","Music, Learning, Swimming, Running, danicing, history countries","Reading","Music, Movies, Reading","Music, Learning, Gaming","Music, Watching football, Movies"),
+  Country = c("Nigeria","Italian living in UK","Nigeria","Ghana","Nigeria","Nigeria"),
+  Discipline =c("Biochemistry","Microalgae Biotechnology and Ecology","Biology","Biomedical Engineering","Plant Biology and Biotechnology","Animal Science"),
+  Prefered_language = c("R","Python","Python","Python","Python","Python"),
+  stringsAsFactors = FALSE
+)
+print("Team Member Information:")
+print(team_data)
+
